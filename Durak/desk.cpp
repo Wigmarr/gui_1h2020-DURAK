e2d659748c2b35e0f6c2a99e34e9d9b660921727
Roman Shestopalov <shystik-pushystik@mail.ru>
2020-05-24 02:30:58 +0300

    Card shuffle fix

--- Durak/desk.cpp
+++ Durak/desk.cpp
@@ -1,19 +1,24 @@
 #include "desk.h"
 #include "card.h"
-#include <random>
-#include <algorithm>
 #include <iterator>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <QDebug>
+
 Desk::Desk()
 {
-    /*int k = 0;
+    int k = 0;
     for (int i=1; i<=4; i++){
         for(int j=6; j<=14; j++){
             this->deck.push_back(Card(i,j));
             k++;
         }
     }
-    std::random_device rd;
-    std::mt19937 g(rd());
-    std::shuffle(this->deck[0], this->deck[35], g);
-    */
+    int size = deck.size();
+    for (int i = 0; i < size - 1; i++) {
+       int j = i + rand() % (size - i);
+       std::swap(deck[i], deck[j]);
+    }
+
 }
